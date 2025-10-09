363fdf78bde0b6db2fa25c1ea0bd915933083957
yuri <yurisantos046@gmail.com>
2025-10-09 11:47:37 -0300

    atualização de EnvConfig

--- src/main/java/com/taskflow/Utils/EnvConfig.java
+++ src/main/java/com/taskflow/Utils/EnvConfig.java
@@ -3,9 +3,15 @@ package com.taskflow.Utils;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class EnvConfig {
-    private static final Dotenv dotenv = Dotenv.load();
+    private static final Dotenv dotenv = Dotenv.configure()
+            .ignoreIfMissing() // ignora se .env não existir
+            .load();
 
     public static String get(String key) {
-        return dotenv.get(key);
+        String value = dotenv.get(key);
+        if (value != null) {
+            return value;
+        }
+        return System.getenv(key); // fallback para variáveis de ambiente do sistema
     }
 }
